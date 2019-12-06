a249cf4a8159a49d93793035ae737ad7b1a0bf06
Rinacm <2653221698@qq.com>
2019-12-07 05:28:13 +0800

    delete redunant code and fix some bugs
    Former-commit-id: e3aea46231b49de85603d70561192448bb8d16c5 [formerly aac2b43fa01123c575de6d91cbdc0b2e560a562f]
    Former-commit-id: 5f1e5dd30c227a3e9a07ea45a326495f034a420f

--- Api/Supplier/IRecursiveContentSupplier.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Pzxlane.Api.Supplier
-{
-    public interface IRecursiveContentSupplier<TEntity, TEntityList> : IPixivSupplier<TEntity, TEntityList>
-    {
-        bool GetCondition(TEntityList param);
-
-        void UpdateCondition(TEntityList param);
-    }
-}
