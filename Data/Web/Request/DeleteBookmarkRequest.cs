2cf1ae0c06f9cd275d1fc2a66a849f00840cd2a8
Rinacm <2653221698@qq.com>
2019-12-12 11:51:26 +0800

    entire home page
    Former-commit-id: faa7af86beebd0bcdaa03b9bfcc4f687b71a291a [formerly 7116982374cb6a97a9c69b2656b3fd47148df89a]
    Former-commit-id: b5ad2d092b10b8d7fef283bffcd36ec9a54ceea9

---
+++ Data/Web/Request/DeleteBookmarkRequest.cs
@@ -0,0 +1,10 @@
+﻿using Refit;
+
+namespace Pixeval.Data.Web.Request
+{
+    public class DeleteBookmarkRequest
+    {
+        [AliasAs("illust_id")]
+        public string IllustId { get; set; }
+    }
+}
---
+++ Objects/TextBuffer.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using System.Text;
+
+namespace Pixeval.Objects
+{
+    public class TextBuffer
+    {
+        public static FileInfo GetOrCreateFile(string path)
+        {
+            if (!File.Exists(path))
+            {
+                File.Create(path);
+            }
+            return new FileInfo(path);
+        }
+    }
+}
