ba13ef888c77f9bad077eb0dce5bdd7c83d0cd33
Rinacm <2653221698@qq.com>
2019-12-19 05:55:15 +0800

    优化代码, 添加查看用户新作功能
    Former-commit-id: d0c441483292ddd91a4e42222cd12d7fdd67c379 [formerly bc6b3188e8dd03d677ad3f6812fe3121e37e0093]
    Former-commit-id: 024b8857daf0b2b310eae41c76dfc0da8e6368b4

---
+++ Objects/Functions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Pixeval.Objects
+{
+    internal static class Functions
+    {
+        public static T Apply<T>(this T receiver, Action<T> action)
+        {
+            action(receiver);
+            return receiver;
+        }
+    }
+}
