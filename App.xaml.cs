f857164fac95d94304eb58a9acbcf5eb310654b4
Dylech30th <2653221698@qq.com>
2020-02-19 23:37:22 +0800

    bug fix

--- App.xaml.cs
+++ App.xaml.cs
@@ -20,6 +20,10 @@ using System.Windows;
 using Pixeval.Persisting;
 using Refit;
 
+#if RELEASE
+using Pixeval.Objects.Exceptions.Logger;
+#endif
+
 namespace Pixeval
 {
     public partial class App
