aca572104bdf5a53c3b1f020aea09962a00d51d8
扑克 <poker_sang@outlook.com>
2021-12-21 22:56:39 +0800

    DependencyProperty自动代码生成

---
+++ src/Pixeval.SourceGen/DependencyProperty/GeneratorHelpers.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
+
+namespace Pixeval.SourceGen.DependencyProperty
+{
+    internal static class GeneratorHelpers
+    {
+        public static TypeSyntax GetTypeSyntax(this ITypeSymbol typeSymbol, bool isNullable)
+        {
+            var typeName = ParseTypeName(typeSymbol.ToDisplayString(SymbolDisplayFormat.MinimallyQualifiedFormat));
+            return isNullable ? NullableType(typeName) : typeName;
+        }
+
+        public static void UseNamespace(this HashSet<string> namespaces, HashSet<ITypeSymbol> usedTypes, INamedTypeSymbol baseClass, ITypeSymbol symbol)
+        {
+            if (usedTypes.Contains(symbol))
+                return;
+
+            usedTypes.Add(symbol);
+
+            var ns = symbol.ContainingNamespace;
+            if (!SymbolEqualityComparer.Default.Equals(ns, baseClass.ContainingNamespace))
+                namespaces.Add(ns.ToDisplayString());
+
+            if (symbol is INamedTypeSymbol { IsGenericType: true } genericSymbol)
+            {
+                foreach (var a in genericSymbol.TypeArguments)
+                    UseNamespace(namespaces, usedTypes, baseClass, a);
+            }
+        }
+    }
+}
---
+++ src/Pixeval/Misc/DependencyPropertyAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Pixeval.Misc;
+
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = false)]
+public sealed class DependencyPropertyAttribute : Attribute
+{
+    public DependencyPropertyAttribute(string name, Type type)
+    {
+        Name = name;
+        Type = type;
+    }
+
+    public string Name { get; }
+    public Type Type { get; }
+    public bool IsSetterPublic { get; init; } = true;
+    public bool IsNullable { get; init; } = true;
+    public string DefaultValue { get; init; } = "null";
+    public bool InstanceChangedCallback { get; init; }
+}
