535399d04821269ff0ce23a2ac5e88830b4ceec3
扑克 <poker_sang@outlook.com>
2022-01-02 15:22:41 +0800

    StandardizeLoadSave

--- src/Pixeval/Attributes/LoadSaveConfigurationAttribute.cs
+++ src/Pixeval/Attributes/LoadSaveConfigurationAttribute.cs
@@ -5,9 +5,12 @@ namespace Pixeval.Attributes;
 [AttributeUsage(AttributeTargets.Class, Inherited = false)]
 public class LoadSaveConfigurationAttribute : Attribute
 {
-    public LoadSaveConfigurationAttribute(Type targetType)
+    public LoadSaveConfigurationAttribute(Type targetType, string containerName)
     {
         TargetType = targetType;
+        ContainerName = containerName;
     }
     public Type TargetType { get; }
+    public string ContainerName { get; }
+    public string CastMethod { get; set; } = "null!";
 }
