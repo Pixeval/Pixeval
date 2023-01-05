4aedee6d0bb2d83167d005c5fdad6ee26462c68b
Dylech30th <decem0730@hotmail.com>
2023-01-05 18:17:05 +0800

    use new LocalizedStringResources attribute

--- src/Pixeval/Strings/StringResources.cs
+++
@@ -1,8 +0,0 @@
-using WinUI3Utilities.Attributes;
-
-// The incorrect namespace declaration is intentional, the Source Generator will use this class and its namespace to generate string resources
-// ReSharper disable once CheckNamespace
-namespace Pixeval;
-
-[LocalizedStringResources]
-file record StringResources;
