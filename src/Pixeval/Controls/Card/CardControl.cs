7a3c3aa46a8ac4f2ea129883bcad6518450973a4
Poker <poker_sang@outlook.com>
2023-07-01 18:01:43 +0800

    优化CardControl

--- src/Pixeval/Controls/Card/CardControl.cs
+++ src/Pixeval/Controls/Card/CardControl.cs
@@ -1,4 +1,4 @@
-﻿#region Copyright (c) Pixeval/Pixeval
+#region Copyright (c) Pixeval/Pixeval
 // GPL v3 License
 // 
 // Pixeval/Pixeval
@@ -18,14 +18,176 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #endregion
 
-using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Automation.Peers;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Input;
+using WinUI3Utilities.Attributes;
 
 namespace Pixeval.Controls.Card;
 
-public sealed class CardControl : ContentControl
+/// <summary>
+/// This is the base control to create consistent settings experiences, inline with the Windows 11 design language.
+/// A CardControl can also be hosted within a SettingsExpander.
+/// </summary>
+[DependencyProperty<bool>("IsClickEnabled", "false", nameof(OnIsClickEnabledChanged))]
+public partial class CardControl : ButtonBase
 {
+    internal const string NormalState = "Normal";
+    internal const string PointerOverState = "PointerOver";
+    internal const string PressedState = "Pressed";
+    internal const string DisabledState = "Disabled";
+
+    /// <summary>
+    /// Creates a new instance of the <see cref="CardControl"/> class.
+    /// </summary>
     public CardControl()
     {
         DefaultStyleKey = typeof(CardControl);
+    }
+
+    private static void OnIsClickEnabledChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        ((CardControl)d).OnIsClickEnabledPropertyChanged((bool)e.OldValue, (bool)e.NewValue);
+    }
+
+    protected virtual void OnIsClickEnabledPropertyChanged(bool oldValue, bool newValue)
+    {
+        OnIsClickEnabledChanged();
+    }
+
+    /// <inheritdoc />
+    protected override void OnApplyTemplate()
+    {
+        base.OnApplyTemplate();
+        IsEnabledChanged -= OnIsEnabledChanged;
+        OnIsClickEnabledChanged();
+        VisualStateManager.GoToState(this, IsEnabled ? NormalState : DisabledState, true);
+        IsEnabledChanged += OnIsEnabledChanged;
+    }
+
+    private void EnableButtonInteraction()
+    {
+        DisableButtonInteraction();
+
+        IsTabStop = true;
+        PointerEntered += Control_PointerEntered;
+        PointerExited += Control_PointerExited;
+        PointerCaptureLost += Control_PointerCaptureLost;
+        PointerCanceled += Control_PointerCanceled;
+        PreviewKeyDown += Control_PreviewKeyDown;
+        PreviewKeyUp += Control_PreviewKeyUp;
+    }
+
+    private void DisableButtonInteraction()
+    {
+        IsTabStop = false;
+        PointerEntered -= Control_PointerEntered;
+        PointerExited -= Control_PointerExited;
+        PointerCaptureLost -= Control_PointerCaptureLost;
+        PointerCanceled -= Control_PointerCanceled;
+        PreviewKeyDown -= Control_PreviewKeyDown;
+        PreviewKeyUp -= Control_PreviewKeyUp;
+    }
+
+    private void Control_PreviewKeyUp(object sender, KeyRoutedEventArgs e)
+    {
+        if (e.Key == Windows.System.VirtualKey.Enter || e.Key == Windows.System.VirtualKey.Space || e.Key == Windows.System.VirtualKey.GamepadA)
+        {
+            VisualStateManager.GoToState(this, NormalState, true);
+        }
+    }
+
+    private void Control_PreviewKeyDown(object sender, KeyRoutedEventArgs e)
+    {
+        if (e.Key == Windows.System.VirtualKey.Enter || e.Key == Windows.System.VirtualKey.Space || e.Key == Windows.System.VirtualKey.GamepadA)
+        {
+            // Check if the active focus is on the card itself - only then we show the pressed state.
+            if (GetFocusedElement() is CardControl)
+            {
+                VisualStateManager.GoToState(this, PressedState, true);
+            }
+        }
+    }
+
+    public void Control_PointerEntered(object sender, PointerRoutedEventArgs e)
+    {
+        base.OnPointerEntered(e);
+        VisualStateManager.GoToState(this, PointerOverState, true);
+    }
+
+    public void Control_PointerExited(object sender, PointerRoutedEventArgs e)
+    {
+        base.OnPointerExited(e);
+        VisualStateManager.GoToState(this, NormalState, true);
+    }
+
+    private void Control_PointerCaptureLost(object sender, PointerRoutedEventArgs e)
+    {
+        base.OnPointerCaptureLost(e);
+        VisualStateManager.GoToState(this, NormalState, true);
+    }
+
+    private void Control_PointerCanceled(object sender, PointerRoutedEventArgs e)
+    {
+        base.OnPointerCanceled(e);
+        VisualStateManager.GoToState(this, NormalState, true);
+    }
+
+    protected override void OnPointerPressed(PointerRoutedEventArgs e)
+    {
+        //  e.Handled = true;
+        if (IsClickEnabled)
+        {
+            base.OnPointerPressed(e);
+            VisualStateManager.GoToState(this, PressedState, true);
+        }
+    }
+
+    protected override void OnPointerReleased(PointerRoutedEventArgs e)
+    {
+        if (IsClickEnabled)
+        {
+            base.OnPointerReleased(e);
+            VisualStateManager.GoToState(this, NormalState, true);
+        }
+    }
+
+    /// <summary>
+    /// Creates AutomationPeer
+    /// </summary>
+    /// <returns>An automation peer for <see cref="CardControl"/>.</returns>
+    protected override AutomationPeer OnCreateAutomationPeer()
+    {
+        return new CardControlAutomationPeer(this);
+    }
+
+    private void OnIsClickEnabledChanged()
+    {
+        if (IsClickEnabled)
+        {
+            EnableButtonInteraction();
+        }
+        else
+        {
+            DisableButtonInteraction();
+        }
+    }
+
+    private void OnIsEnabledChanged(object sender, DependencyPropertyChangedEventArgs e)
+    {
+        VisualStateManager.GoToState(this, IsEnabled ? NormalState : DisabledState, true);
+    }
+
+    private FrameworkElement? GetFocusedElement()
+    {
+        if (XamlRoot != null)
+        {
+            return FocusManager.GetFocusedElement(XamlRoot) as FrameworkElement;
+        }
+        else
+        {
+            return FocusManager.GetFocusedElement() as FrameworkElement;
+        }
     }
 }
