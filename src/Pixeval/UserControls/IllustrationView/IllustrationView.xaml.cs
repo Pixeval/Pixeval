a134c2ac083629583ad0416cedd1fa8a3f2772ed
Poker <poker_sang@outlook.com>
2023-07-15 05:16:03 +0800

    简化动画

--- src/Pixeval/UserControls/IllustrationView/IllustrationView.xaml.cs
+++ src/Pixeval/UserControls/IllustrationView/IllustrationView.xaml.cs
@@ -25,7 +25,6 @@ using Microsoft.UI.Input;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Media.Animation;
 using Pixeval.Controls;
 using Pixeval.Options;
@@ -61,12 +60,6 @@ public sealed partial class IllustrationView
         d.To<UserControl>().Resources[nameof(Box)].To<Box>().Tag = value;
     }
 
-    private static readonly ExponentialEase _imageSourceSetEasingFunction = new()
-    {
-        EasingMode = EasingMode.EaseOut,
-        Exponent = 12
-    };
-
     public IllustrationView()
     {
         InitializeComponent();
@@ -74,13 +67,9 @@ public sealed partial class IllustrationView
         ViewModel.DataProvider.FilterChanged += (sender, _) =>
         {
             if (sender is Predicate<object> predicate)
-            {
                 ViewModel.DataProvider.View.Filter = predicate;
-            }
             else
-            {
                 ViewModel.DataProvider.View.Refresh();
-            }
             LoadMoreIfNeeded().Discard();
         };
     }
@@ -93,7 +82,12 @@ public sealed partial class IllustrationView
 
     public IllustrationViewViewModel ViewModel => _viewModelRef.Value;
 
-    private async void ToggleBookmarkButton_OnTapped(object sender, TappedRoutedEventArgs e)
+    private void IllustrationViewOnUnloaded(object sender, RoutedEventArgs e)
+    {
+        _viewModelRef.Dispose();
+    }
+
+    private async void ToggleBookmarkButtonOnTapped(object sender, TappedRoutedEventArgs e)
     {
         e.Handled = true;
         var viewModel = sender.GetDataContext<IllustrationViewModel>();
@@ -103,7 +97,7 @@ public sealed partial class IllustrationView
             await viewModel.PostPublicBookmarkAsync();
     }
 
-    private void Thumbnail_OnTapped(object sender, TappedRoutedEventArgs e)
+    private void ThumbnailOnTapped(object sender, TappedRoutedEventArgs e)
     {
         if (InputKeyboardSource.GetKeyStateForCurrentThread(VirtualKey.Shift).HasFlag(CoreVirtualKeyStates.Down))
         {
@@ -127,48 +121,48 @@ public sealed partial class IllustrationView
             .WithSizeLimit(640, 360)
             .Init(new(width, height))
             .Activate();
-    }
 
-    private static (int windowWidth, int windowHeight) DetermineWindowSize(int illustWidth, double illustRatio)
-    {
-        /*
-        var windowHandle = User32.MonitorFromWindow((nint)CurrentContext.HWnd, User32.MonitorOptions.MONITOR_DEFAULTTONEAREST);
-        User32.GetMonitorInfo(windowHandle, out var monitorInfoEx);
-        var devMode = DEVMODE.Create();
-        while (!User32.EnumDisplaySettings(
-                   monitorInfoEx.DeviceName,
-                   User32.ENUM_CURRENT_SETTINGS,
-                   &devMode))
-        { }
-
-        var monitorWidth = devMode.dmPelsWidth;
-        var monitorHeight = devMode.dmPelsHeight;
-        */
-
-        var (monitorWidth, monitorHeight) = WindowHelper.GetScreenSize();
-
-        var determinedWidth = illustWidth switch
+        static (int windowWidth, int windowHeight) DetermineWindowSize(int illustWidth, double illustRatio)
         {
-            not 1500 => 1500 + Random.Shared.Next(0, 200),
-            _ => 1500
-        };
-        var windowWidth = determinedWidth > monitorWidth ? (int)monitorWidth - 100 : determinedWidth;
-        // 51 is determined through calculation, it is the height of the title bar
-        var windowHeight = windowWidth / illustRatio + 51 is var height && height > monitorHeight - 80 // 80: estimated working area height
-            ? monitorHeight - 100
-            : height;
-        return (windowWidth, (int)windowHeight);
+            /*
+            var windowHandle = User32.MonitorFromWindow((nint)CurrentContext.HWnd, User32.MonitorOptions.MONITOR_DEFAULTTONEAREST);
+            User32.GetMonitorInfo(windowHandle, out var monitorInfoEx);
+            var devMode = DEVMODE.Create();
+            while (!User32.EnumDisplaySettings(
+                       monitorInfoEx.DeviceName,
+                       User32.ENUM_CURRENT_SETTINGS,
+                       &devMode))
+            { }
+
+            var monitorWidth = devMode.dmPelsWidth;
+            var monitorHeight = devMode.dmPelsHeight;
+            */
+
+            var (monitorWidth, monitorHeight) = WindowHelper.GetScreenSize();
+
+            var determinedWidth = illustWidth switch
+            {
+                not 1500 => 1500 + Random.Shared.Next(0, 200),
+                _ => 1500
+            };
+            var windowWidth = determinedWidth > monitorWidth ? (int)monitorWidth - 100 : determinedWidth;
+            // 51 is determined through calculation, it is the height of the title bar
+            var windowHeight = windowWidth / illustRatio + 51 is var height && height > monitorHeight - 80 // 80: estimated working area height
+                ? monitorHeight - 100
+                : height;
+            return (windowWidth, (int)windowHeight);
+        }
     }
 
     /// <summary>
     /// For Bookmark Button
     /// </summary>
-    private void IllustrationThumbnailContainerItem_OnTapped(object sender, TappedRoutedEventArgs e)
+    private void IllustrationThumbnailContainerItemOnTapped(object sender, TappedRoutedEventArgs e)
     {
         e.Handled = true;
     }
 
-    private async void IllustrationThumbnailContainerItem_OnEffectiveViewportChanged(FrameworkElement sender, EffectiveViewportChangedEventArgs args)
+    private async void IllustrationThumbnailContainerItemOnEffectiveViewportChanged(FrameworkElement sender, EffectiveViewportChangedEventArgs args)
     {
         var context = sender.GetDataContext<IllustrationViewModel>();
         var preLoadRows = Math.Clamp(App.AppViewModel.AppSetting.PreLoadRows, 1, 15);
@@ -178,20 +172,10 @@ public sealed partial class IllustrationView
             var option = IllustrationViewOption.ToThumbnailUrlOption();
             if (await context.LoadThumbnailIfRequired(option))
             {
-                var transform = (ScaleTransform)sender.RenderTransform;
                 if (sender.IsFullyOrPartiallyVisible(this))
-                {
-                    var scaleXAnimation = transform.CreateDoubleAnimation(nameof(transform.ScaleX), from: 1.1, to: 1, easingFunction: _imageSourceSetEasingFunction, duration: TimeSpan.FromSeconds(2));
-                    var scaleYAnimation = transform.CreateDoubleAnimation(nameof(transform.ScaleY), from: 1.1, to: 1, easingFunction: _imageSourceSetEasingFunction, duration: TimeSpan.FromSeconds(2));
-                    var opacityAnimation = sender.CreateDoubleAnimation(nameof(sender.Opacity), from: 0, to: 1, easingFunction: _imageSourceSetEasingFunction, duration: TimeSpan.FromSeconds(2));
-                    UIHelper.CreateStoryboard(scaleXAnimation, scaleYAnimation, opacityAnimation).Begin();
-                }
+                    sender.Resources["IllustrationThumbnailContainerItemStoryboard"].To<Storyboard>().Begin();
                 else
-                {
-                    transform.ScaleX = 1;
-                    transform.ScaleY = 1;
                     sender.Opacity = 1;
-                }
             }
 
             return;
@@ -208,49 +192,54 @@ public sealed partial class IllustrationView
         // }
     }
 
+    private void ScrollViewerViewChanged(object? sender, ScrollViewerViewChangedEventArgs e)
+    {
+        LoadMoreIfNeeded().Discard();
+    }
+
     public async Task LoadMoreIfNeeded(uint number = 20)
     {
         // TODO load after being Filtrated
         if (ScrollViewer.ScrollableHeight - LoadingArea.ActualHeight < ScrollViewer.VerticalOffset)
-            await ViewModel.DataProvider.View.LoadMoreItemsAsync(number);
+            _ = await ViewModel.DataProvider.View.LoadMoreItemsAsync(number);
     }
 
-    private void BookmarkContextItem_OnTapped(object sender, TappedRoutedEventArgs e)
+    private async void BookmarkContextItemOnTapped(object sender, TappedRoutedEventArgs e)
     {
-        sender.GetDataContext<IllustrationViewModel>().SwitchBookmarkStateAsync();
+        await sender.GetDataContext<IllustrationViewModel>().SwitchBookmarkStateAsync();
     }
 
-    private async void SaveContextItem_OnTapped(object sender, TappedRoutedEventArgs e)
+    private async void SaveContextItemOnTapped(object sender, TappedRoutedEventArgs e)
     {
         await sender.GetDataContext<IllustrationViewModel>().SaveAsync();
     }
 
-    private async void SaveAsContextItem_OnTapped(object sender, TappedRoutedEventArgs e)
+    private async void SaveAsContextItemOnTapped(object sender, TappedRoutedEventArgs e)
     {
         await sender.GetDataContext<IllustrationViewModel>().SaveAsAsync();
     }
 
-    private async void OpenInBrowserContextItem_OnTapped(object sender, TappedRoutedEventArgs e)
+    private async void OpenInBrowserContextItemOnTapped(object sender, TappedRoutedEventArgs e)
     {
-        await Launcher.LaunchUriAsync(MakoHelper.GenerateIllustrationWebUri(sender.GetDataContext<IllustrationViewModel>().Id));
+        _ = await Launcher.LaunchUriAsync(MakoHelper.GenerateIllustrationWebUri(sender.GetDataContext<IllustrationViewModel>().Id));
     }
 
-    private void AddToBookmarkContextItem_OnTapped(object sender, TappedRoutedEventArgs e)
+    private void AddToBookmarkContextItemOnTapped(object sender, TappedRoutedEventArgs e)
     {
         throw new NotImplementedException();
     }
 
-    private void CopyWebLinkContextItem_OnTapped(object sender, TappedRoutedEventArgs e)
+    private void CopyWebLinkContextItemOnTapped(object sender, TappedRoutedEventArgs e)
     {
         UIHelper.ClipboardSetText(MakoHelper.GenerateIllustrationWebUri(sender.GetDataContext<IllustrationViewModel>().Id).ToString());
     }
 
-    private void CopyAppLinkContextItem_OnTapped(object sender, TappedRoutedEventArgs e)
+    private void CopyAppLinkContextItemOnTapped(object sender, TappedRoutedEventArgs e)
     {
         UIHelper.ClipboardSetText(MakoHelper.GenerateIllustrationAppUri(sender.GetDataContext<IllustrationViewModel>().Id).ToString());
     }
 
-    private async void ShowQrCodeContextItem_OnTapped(object sender, TappedRoutedEventArgs e)
+    private async void ShowQrCodeContextItemOnTapped(object sender, TappedRoutedEventArgs e)
     {
         var webQrCodeSource = await UIHelper.GenerateQrCodeAsync(MakoHelper.GenerateIllustrationWebUri(sender.GetDataContext<IllustrationViewModel>().Id).ToString());
         QrCodeTeachingTip.HeroContent.To<Image>().Source = webQrCodeSource;
@@ -265,7 +254,7 @@ public sealed partial class IllustrationView
         QrCodeTeachingTip.Closed += Closed;
     }
 
-    private async void ShowPixEzQrCodeContextItem_OnTapped(object sender, TappedRoutedEventArgs e)
+    private async void ShowPixEzQrCodeContextItemOnTapped(object sender, TappedRoutedEventArgs e)
     {
         var pixEzQrCodeSource = await UIHelper.GenerateQrCodeAsync(MakoHelper.GenerateIllustrationPixEzUri(sender.GetDataContext<IllustrationViewModel>().Id).ToString());
         QrCodeTeachingTip.HeroContent.To<Image>().Source = pixEzQrCodeSource;
@@ -278,15 +267,5 @@ public sealed partial class IllustrationView
         }
 
         QrCodeTeachingTip.Closed += Closed;
-    }
-
-    private void ScrollViewer_ViewChanged(object? sender, ScrollViewerViewChangedEventArgs e)
-    {
-        LoadMoreIfNeeded().Discard();
-    }
-
-    private void IllustrationView_OnUnloaded(object sender, RoutedEventArgs e)
-    {
-        _viewModelRef.Dispose();
     }
 }
