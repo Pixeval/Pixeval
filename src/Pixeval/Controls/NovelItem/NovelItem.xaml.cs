10f7c5a487bddc9b9208e831d66721eeaa68b42a
Poker <poker_sang@outlook.com>
2024-03-20 19:50:18 +0800

    临时修复 (#395)
    * 修复文件缓存bug
    
    * 修复动画和加载效果

--- src/Pixeval/Controls/NovelItem/NovelItem.xaml.cs
+++ src/Pixeval/Controls/NovelItem/NovelItem.xaml.cs
@@ -35,20 +35,18 @@ public sealed partial class NovelItem
         {
             var old = _isPointerOver;
             _isPointerOver = value;
+            var currentView = ConnectedAnimationService.GetForCurrentView();
             if (IsPointerOver > 0 && old <= 0)
             {
-                var anim1 = ConnectedAnimationService.GetForCurrentView().PrepareToAnimate("ForwardConnectedAnimation1", this);
-                var anim2 = ConnectedAnimationService.GetForCurrentView().PrepareToAnimate("ForwardConnectedAnimation2", Image);
-                var anim3 = ConnectedAnimationService.GetForCurrentView().PrepareToAnimate("ForwardConnectedAnimation3", HeartButton);
-                var anim4 = ConnectedAnimationService.GetForCurrentView().PrepareToAnimate("ForwardConnectedAnimation4", TitleTextBlock);
-                var anim5 = ConnectedAnimationService.GetForCurrentView().PrepareToAnimate("ForwardConnectedAnimation5", AuthorTextBlock);
-                var anim6 = ConnectedAnimationService.GetForCurrentView().PrepareToAnimate("ForwardConnectedAnimation6", TagsList);
-                anim1.Configuration = new BasicConnectedAnimationConfiguration();
-                anim2.Configuration = new BasicConnectedAnimationConfiguration();
-                anim3.Configuration = new BasicConnectedAnimationConfiguration();
-                anim4.Configuration = new BasicConnectedAnimationConfiguration();
-                anim5.Configuration = new BasicConnectedAnimationConfiguration();
-                anim6.Configuration = new BasicConnectedAnimationConfiguration();
+                var anim1 = currentView.PrepareToAnimate("ForwardConnectedAnimation1", this);
+                var anim2 = currentView.PrepareToAnimate("ForwardConnectedAnimation2", Image);
+                var anim3 = currentView.PrepareToAnimate("ForwardConnectedAnimation3", HeartButton);
+                var anim4 = currentView.PrepareToAnimate("ForwardConnectedAnimation4", TitleTextBlock);
+                var anim5 = currentView.PrepareToAnimate("ForwardConnectedAnimation5", AuthorTextBlock);
+                var anim6 = currentView.PrepareToAnimate("ForwardConnectedAnimation6", TagsList);
+                anim1.Configuration = anim2.Configuration = anim3.Configuration =
+                    anim4.Configuration = anim5.Configuration = anim6.Configuration =
+                        new BasicConnectedAnimationConfiguration();
                 _ = anim1.TryStart(NovelItemPopup);
                 _ = anim2.TryStart(PopupImage);
                 _ = anim3.TryStart(PopupHeartButton);
@@ -56,14 +54,32 @@ public sealed partial class NovelItem
                 _ = anim5.TryStart(PopupAuthorTextBlock);
                 _ = anim6.TryStart(PopupTagsList);
                 NovelItemPopup.Child.To<FrameworkElement>().Width = ActualWidth + 10;
+                NovelItemPopup.IsOpen = true;
             }
-
-            if (IsPointerOver <= 0 && old > 0)
+            else if (IsPointerOver <= 0 && old > 0)
             {
-                // TODO: Backward connected animation
+                var anim1 = currentView.PrepareToAnimate("BackwardConnectedAnimation1", NovelItemPopup);
+                var anim2 = currentView.PrepareToAnimate("BackwardConnectedAnimation2", PopupImage);
+                var anim3 = currentView.PrepareToAnimate("BackwardConnectedAnimation3", PopupHeartButton);
+                var anim4 = currentView.PrepareToAnimate("BackwardConnectedAnimation4", PopupTitleTextBlock);
+                var anim5 = currentView.PrepareToAnimate("BackwardConnectedAnimation5", PopupAuthorTextBlock);
+                var anim6 = currentView.PrepareToAnimate("BackwardConnectedAnimation6", PopupTagsList);
+                anim1.Configuration = anim2.Configuration = anim3.Configuration =
+                    anim4.Configuration = anim5.Configuration = anim6.Configuration =
+                        new BasicConnectedAnimationConfiguration();
+                anim1.Completed += (_, _) =>
+                {
+                    NovelItemPopup.IsOpen = false;
+                    NovelItemPopup.Visibility = Visibility.Visible;
+                };
+                _ = anim1.TryStart(this);
+                _ = anim2.TryStart(Image);
+                _ = anim3.TryStart(HeartButton);
+                _ = anim4.TryStart(TitleTextBlock);
+                _ = anim5.TryStart(AuthorTextBlock);
+                _ = anim6.TryStart(TagsList);
+                _ = Task.Delay(100).ContinueWith(_ => NovelItemPopup.Visibility = Visibility.Collapsed, TaskScheduler.FromCurrentSynchronizationContext());
             }
-
-            NovelItemPopup.IsOpen = IsPointerOver > 0;
         }
     }
 
