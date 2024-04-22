7bdc50012cbd7b6b558959c067d4b0990ac5b76e
Poker <poker_sang@outlook.com>
2024-04-22 13:32:12 +0800

    修复一些杂项崩溃问题

--- src/Pixeval/Pages/Tags/TagsEntryViewModel.cs
+++ src/Pixeval/Pages/Tags/TagsEntryViewModel.cs
@@ -32,7 +32,7 @@ public partial class TagsEntryViewModel : ObservableObject, IEntry, IDisposable
     /// <remarks>
     /// Should be private set
     /// </remarks>
-    [ObservableProperty] private SoftwareBitmapSource _thumbnail = null!;
+    [ObservableProperty] private SoftwareBitmapSource? _thumbnail;
 
     private FrozenSet<string>? _tagsSet;
 
@@ -127,5 +127,5 @@ public partial class TagsEntryViewModel : ObservableObject, IEntry, IDisposable
         entry.Thumbnail = await (await IoHelper.GetFileThumbnailAsync(path)).GetSoftwareBitmapSourceAsync(true);
     }
 
-    public void Dispose() => Thumbnail.Dispose();
+    public void Dispose() => Thumbnail?.Dispose();
 }
