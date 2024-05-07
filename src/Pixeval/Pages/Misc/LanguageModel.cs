e8ba51bb2b55da524ad3ac8f21706d509e341087
Poker <poker_sang@outlook.com>
2024-05-08 01:23:55 +0800

    优化语言设置，在登录页添加语言选项 (#453)

--- src/Pixeval/Pages/Misc/LanguageModel.cs
+++ src/Pixeval/Pages/Misc/LanguageModel.cs
@@ -1,3 +1,5 @@
+using Windows.Globalization;
+
 namespace Pixeval.Pages.Misc;
 
 public record LanguageModel(string DisplayName, string Name)
@@ -5,4 +7,10 @@ public record LanguageModel(string DisplayName, string Name)
     public override string ToString() => DisplayName;
 
     public static LanguageModel DefaultLanguage { get; } = new(SettingsPageResources.LanguageSystemDefault, "");
+
+    public static LanguageModel FromBcl47(string bcp47)
+    {
+        var language = new Language(bcp47);
+        return new LanguageModel(language.NativeName, language.LanguageTag);
+    }
 }
