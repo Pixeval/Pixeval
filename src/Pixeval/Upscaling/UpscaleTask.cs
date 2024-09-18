d0d605851536be06faba81aef3e0330b02bb3cb4
Dylech30th <decem0730@hotmail.com>
2024-09-19 06:55:11 +0800

    initial commit

---
+++ src/Pixeval/Upscaling/UpscaleTask.cs
@@ -0,0 +1,32 @@
+#region Copyright (c) Pixeval/Pixeval
+// GPL v3 License
+// 
+// Pixeval/Pixeval
+// Copyright (c) 2024 Pixeval/UpscalingTask.cs
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#endregion
+
+using System.IO;
+
+namespace Pixeval.Upscaling;
+
+public enum RealESRGANModel
+{
+    RealESRGANX4Plus,
+    RealESRNETX4Plus,
+    RealESRGANX4PlusAnime
+}
+
+public record UpscaleTask(Stream ImageStream, RealESRGANModel Model);
