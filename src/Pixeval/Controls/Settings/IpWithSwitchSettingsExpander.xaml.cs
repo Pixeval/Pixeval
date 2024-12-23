fd65b39f18a54a9c4104e57f55a32fd8e08780f8
Poker <poker_sang@outlook.com>
2024-12-23 15:45:51 +0800

    优化IP输入体验

--- src/Pixeval/Controls/Settings/IpWithSwitchSettingsExpander.xaml.cs
+++ src/Pixeval/Controls/Settings/IpWithSwitchSettingsExpander.xaml.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Pixeval.Settings.Models;
