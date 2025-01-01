e54e788007df7afa6fb7963d696d19982f038500
Poker <poker_sang@outlook.com>
2025-01-01 13:50:51 +0800

    更新 (#592)

--- src/Pixeval/Pages/Misc/DependencyViewModel.cs
+++ src/Pixeval/Pages/Misc/DependencyViewModel.cs
@@ -6,16 +6,12 @@ namespace Pixeval.Pages.Misc;
 public record DependencyViewModel(string Name, string Author, string Url, string License)
 {
     // ReSharper disable StringLiteralTypo
-    private static readonly (string, string)[] _dependencies =
+    private static readonly (string, string)[] _Dependencies =
     [
         ("CommunityToolkit", LicenseTexts.MIT("2024", ".NET Foundation and Contributors.")),
         ("mysticmind/reversemarkdown-net", LicenseTexts.MIT("2015", "Babu Annamalai")),
-        ("GitTools/GitVersion", LicenseTexts.MIT("2021", "NServiceBus Ltd, GitTools and contributors.")),
         ("Poker-sang/WinUI3Utilities", LicenseTexts.MIT("2023", "Poker-sang")),
-        ("dotnet/runtime/tree/main/src/libraries/Microsoft.Extensions.DependencyInjection", LicenseTexts.MIT("2024", ".NET Foundation and Contributors.")),
         ("codebude/QRCoder", LicenseTexts.MIT("2013-2018", "Raffael Herrmann")),
-        ("microsoft/Microsoft.IO.RecyclableMemoryStream", LicenseTexts.MIT("2015-2016", "Microsoft")),
-        ("microsoft/Win2D", LicenseTexts.MIT(string.Empty, "Microsoft Corporation. All rights reserved")),
         ("Sergio0694/PolySharp", LicenseTexts.MIT("2022", "Sergio Pedri")),
         ("SixLabors/ImageSharp", LicenseTexts.ImageSharp),
         ("xinntao/Real-ESRGAN", LicenseTexts.BSD3("2021", "Xintao Wang")),
@@ -28,7 +24,7 @@ public record DependencyViewModel(string Name, string Author, string Url, string
     // ReSharper restore StringLiteralTypo
 
     public static IEnumerable<DependencyViewModel> DependencyViewModels =>
-        _dependencies.Select(t =>
+        _Dependencies.Select(t =>
         {
             var segments = t.Item1.Split('/');
             return new DependencyViewModel(segments[^1], "by " + segments[0], "https://github.com/" + t.Item1, t.Item2);
