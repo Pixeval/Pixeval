ce83e5b481556e95120aafa0935e0cfe984ec035
Poker <poker_sang@outlook.com>
2025-01-01 12:05:00 +0800

    更换ThemeResource

--- src/Pixeval.Controls/IconText.xaml.cs
+++ src/Pixeval.Controls/IconText.xaml.cs
@@ -1,13 +1,10 @@
 using FluentIcons.Common;
-using Microsoft.UI.Xaml.Media;
 using WinUI3Utilities.Attributes;
 
 namespace Pixeval.Controls;
 
 [DependencyProperty<Symbol>("Symbol")]
 [DependencyProperty<string>("Text")]
-[DependencyProperty<Brush>("IconForeground")]
-[DependencyProperty<Brush>("TextForeground")]
 public sealed partial class IconText
 {
     public IconText() => InitializeComponent();
