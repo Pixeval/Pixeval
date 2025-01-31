97824aa3aa04407788be2ffadc6b5ec647c1fdf6
Summpot <summpot@outlook.com>
2025-02-01 00:17:54 +0800

    fix

--- src/Pixeval.SourceGen/StructuralDisposalHookDetector.cs
+++ src/Pixeval.SourceGen/StructuralDisposalHookDetector.cs
@@ -10,7 +10,7 @@ using Microsoft.CodeAnalysis.Diagnostics;
 namespace Pixeval.SourceGen
 {
     [Generator(LanguageNames.CSharp)]
-    public class StructuralDisposalHookAnalyzer : IIncrementalGenerator
+    public class StructuralDisposalHookDetector : IIncrementalGenerator
     {
         public const string DiagnosticId = "PE1001";
 
@@ -24,11 +24,14 @@ namespace Pixeval.SourceGen
 
         public void Initialize(IncrementalGeneratorInitializationContext context)
         {
+            var enableDetector = context.AnalyzerConfigOptionsProvider.Select(static (options, _) =>
+                options.GlobalOptions.TryGetValue("structural_disposal_hook_detection", out var enableDetector) && enableDetector.Equals("true", StringComparison.InvariantCultureIgnoreCase));
+
             var compilation = context.CompilationProvider;
 
             var typeDeclarations = context.SyntaxProvider.CreateSyntaxProvider(
-                predicate: static (node, _) => node is ClassDeclarationSyntax or StructDeclarationSyntax,
-                transform: static (syntaxContext, _) => (TypeDeclarationSyntax) syntaxContext.Node);
+                predicate: static (node, _) => node is TypeDeclarationSyntax,
+                transform: static (syntaxContext, _) => syntaxContext);
 
             var typeSymbols = context.CompilationProvider.SelectMany(((compilation1, token) =>
             {
@@ -63,13 +66,12 @@ namespace Pixeval.SourceGen
             var typeSymbolsImplementTargetInterface = typeSymbols.Combine(targetInterface)
                 .Where(tuple => tuple.Left.AllInterfaces.Contains(tuple.Right, SymbolEqualityComparer.Default)).Select((tuple, _) => tuple.Left);
 
-            var typesImplementTargetInterface = typeDeclarations.Combine(compilation.Combine(targetInterface)).Select(
+            var typesImplementTargetInterface = typeDeclarations.Combine(targetInterface).Select(
                 (tuple, _) =>
                 {
-                    var typeDeclarationSyntax = tuple.Left;
-                    var semanticModel = tuple.Right.Left.GetSemanticModel(typeDeclarationSyntax.SyntaxTree);
-                    var targetInterface1 = tuple.Right.Right;
-                    var typeSymbol = semanticModel.GetDeclaredSymbol(typeDeclarationSyntax);
+                    var (syntaxContext, targetInterface1) = tuple;
+                    var typeDeclarationSyntax = (TypeDeclarationSyntax) syntaxContext.Node;
+                    var typeSymbol = syntaxContext.SemanticModel.GetDeclaredSymbol(typeDeclarationSyntax);
                     return (typeDeclarationSyntax, typeSymbol, targetInterface1);
                 }).Where(tuple =>
             {
@@ -90,48 +92,58 @@ namespace Pixeval.SourceGen
             var invocations = context.SyntaxProvider
                 .CreateSyntaxProvider(
                     predicate: static (node, _) => node is InvocationExpressionSyntax,
-                    transform: static (syntaxContext, _) => (InvocationExpressionSyntax) syntaxContext.Node);
+                    transform: static (syntaxContext, _) => syntaxContext);
 
             var targetMethod =
-                targetInterface.Select((interface1, _) => interface1.GetMembers(TargetMethodName).Single());
+                targetInterface.Select((interface1, _) => interface1.GetMembers(TargetMethodName).SingleOrDefault());
 
-            var invocationsWithTargetMethod = invocations.Combine(compilation.Combine(targetMethod)).Where(tuple =>
+            var invocationsWithTargetMethod = invocations.Combine(targetMethod).Where(tuple =>
             {
-                var invocation = tuple.Left;
-                var semanticModel = tuple.Right.Left.GetSemanticModel(invocation.SyntaxTree);
-                var targetMethod1 = tuple.Right.Right;
-                var symbolInfo = semanticModel.GetSymbolInfo(invocation);
+                var (syntaxContext, targetMethod1) = tuple;
+                if (targetMethod1 is null)
+                {
+                    return false;
+                }
+                var invocation = (InvocationExpressionSyntax) syntaxContext.Node;
+                var symbolInfo = syntaxContext.SemanticModel.GetSymbolInfo(invocation);
                 if (symbolInfo.Symbol is IMethodSymbol methodSymbol)
                 {
                     return SymbolEqualityComparer.Default.Equals(methodSymbol, targetMethod1);
                 }
 
                 return false;
-            }).Select((tuple, _) => tuple.Left);
+            }).Select((tuple, _) => tuple.Left.Node);
 
-            var typesWithTargetMethodInvocation = typeSymbolsImplementTargetInterface
+            var typesWithoutTargetMethodInvocation = typeSymbolsImplementTargetInterface
                 .Combine((typesImplementTargetInterface.Collect().Combine(invocationsWithTargetMethod.Collect())))
                 .Where(tuple =>
                 {
-                    var typeSymbol = tuple.Left;
-                    var types = tuple.Right.Left;
-                    var invocations1 = tuple.Right.Right;
-                    if (invocations1.IsEmpty)
-                    {
-                        return true;
-                    }
+                    var (typeSymbol, (types, invocations1)) = tuple;
+
 
                     var typeDeclarations1 =
                         types.Where(tuple1 => SymbolEqualityComparer.Default.Equals(tuple1.typeSymbol, typeSymbol))
                             .Select(tuple1 => tuple1.typeDeclarationSyntax).ToImmutableArray();
+                    if (typeDeclarations1.IsDefaultOrEmpty)
+                    {
+                        return false;
+                    }
+                    if (invocations1.IsDefaultOrEmpty)
+                    {
+                        return true;
+                    }
                     return !typeDeclarations1.Any(typeDeclaration => invocations1.Any(invocation =>
                         invocation.FirstAncestorOrSelf<TypeDeclarationSyntax>(node => node == typeDeclaration) is not
                             null));
-                }).Select((tuple, _) => tuple.Left).Combine(compilation);
+                }).Select((tuple, _) => tuple.Left);
 
-            context.RegisterSourceOutput(typesWithTargetMethodInvocation, (spc, tuple) =>
+            context.RegisterSourceOutput(typesWithoutTargetMethodInvocation.Combine(enableDetector), (spc, source) =>
             {
-                var typeSymbol = tuple.Left;
+                if (!source.Right)
+                {
+                    return;
+                }
+                var type = source.Left;
                 var diagnostic = Diagnostic.Create(
                     new DiagnosticDescriptor(
                         id: DiagnosticId,
@@ -140,7 +152,7 @@ namespace Pixeval.SourceGen
                         category: Category,
                         defaultSeverity: DiagnosticSeverity.Error,
                         isEnabledByDefault: true),
-                    Location.None, typeSymbol.Name);
+                    Location.None, type.Name);
 
                 spc.ReportDiagnostic(diagnostic);
             });
