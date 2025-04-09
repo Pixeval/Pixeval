90c1e0b78e7eb4607d115823dc30c4ec3eb29708
Poker <poker_sang@outlook.com>
2025-04-10 02:15:32 +0800

    添加标签分类

--- src/Pixeval/Pages/WorkViewer/WorkInfoPageViewModel.cs
+++ src/Pixeval/Pages/WorkViewer/WorkInfoPageViewModel.cs
@@ -1,28 +1,12 @@
 // Copyright (c) Pixeval.
 // Licensed under the GPL v3 License.
 
-using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
-using Microsoft.UI.Xaml.Media;
-using Mako.Model;
-using Pixeval.Util.IO.Caching;
+using Misaki;
 
 namespace Pixeval.Pages;
 
-public partial class WorkInfoPageViewModel<T>(T entry) : ObservableObject where T : class, IWorkEntry
+public partial class WorkInfoPageViewModel(IArtworkInfo entry) : ObservableObject
 {
-    public T Entry { get; } = entry;
-
-    public UserEntity Illustrator { get; } = entry.User;
-
-    public string? IllustrationDimensionText => Entry is Illustration illustration ? $"{illustration.Width} x {illustration.Height}" : null;
-
-    [ObservableProperty]
-    public partial ImageSource? AvatarSource { get; set; }
-
-    public async Task LoadAvatarAsync()
-    {
-        if (Illustrator is { ProfileImageUrls.Medium: { } profileImage })
-            AvatarSource = await CacheHelper.GetSourceFromCacheAsync(profileImage);
-    }
+    public IArtworkInfo Entry { get; } = entry;
 }
