cc497fb11c6d30ba7146b6386f61a6567d79acb2
Poker <poker_sang@outlook.com>
2025-08-01 18:44:33 +0800

    简化域前置代码

--- src/Pixeval.Utilities/PixivAuthenticationProxyServer.cs
+++ src/Pixeval.Utilities/PixivAuthenticationProxyServer.cs
@@ -15,15 +15,15 @@ namespace Pixeval.Utilities;
 
 public partial class PixivAuthenticationProxyServer : IDisposable
 {
-    private readonly X509Certificate2? _certificate;
-    private readonly TcpListener? _tcpListener;
-    private readonly Func<string, Task<IPAddress[]>> _dns;
+    private readonly X509Certificate2 _certificate;
+    private readonly TcpListener _tcpListener;
+    private readonly Func<string, Task<SslStream>> _createConnectionAsync;
 
-    private PixivAuthenticationProxyServer(X509Certificate2 certificate, TcpListener tcpListener, Func<string, Task<IPAddress[]>> dns)
+    private PixivAuthenticationProxyServer(X509Certificate2 certificate, TcpListener tcpListener, Func<string, Task<SslStream>> createConnectionAsync)
     {
         _certificate = certificate;
         _tcpListener = tcpListener;
-        _dns = dns;
+        _createConnectionAsync = createConnectionAsync;
         _tcpListener.Start();
         _ = _tcpListener.BeginAcceptTcpClient(AcceptTcpClientCallback, _tcpListener);
     }
@@ -35,72 +35,48 @@ public partial class PixivAuthenticationProxyServer : IDisposable
         _tcpListener?.Stop();
     }
 
-    public static PixivAuthenticationProxyServer Create(IPAddress ip, int port, Func<string, Task<IPAddress[]>> dns, X509Certificate2? certificate = null)
+    public static PixivAuthenticationProxyServer Create(IPAddress ip, int port, Func<string, Task<SslStream>> createConnectionAsync)
     {
         var now = DateTimeOffset.UtcNow;
         using var tempCert = CertGenerator.CreateCACertificate(null, now, now + TimeSpan.FromDays(3));
         var cert = new X509Certificate2(tempCert.Export(X509ContentType.Pkcs12, ""), "", X509KeyStorageFlags.UserKeySet);
-        return new(cert, new(ip, port), dns);
+        return new(cert, new(ip, port), createConnectionAsync);
     }
 
     private async void AcceptTcpClientCallback(IAsyncResult result)
     {
         try
         {
-            if (result.AsyncState is TcpListener listener)
-            {
-                using var client = listener.EndAcceptTcpClient(result);
-                _ = listener.BeginAcceptTcpClient(AcceptTcpClientCallback, listener);
-                using (client)
-                {
-                    var clientStream = client.GetStream();
-                    var content = await new StreamReader(clientStream).ReadLineAsync();
-                    // content starts with "CONNECT" means it's trying to establish an HTTPS connection
-                    if (content is null || !content.StartsWith("CONNECT"))
-                    {
-                        return;
-                    }
+            if (result.AsyncState is not TcpListener listener)
+                return;
+            using var client = listener.EndAcceptTcpClient(result);
+            _ = listener.BeginAcceptTcpClient(AcceptTcpClientCallback, listener);
+            var clientStream = client.GetStream();
+            var content = await new StreamReader(clientStream).ReadLineAsync();
+            // content starts with "CONNECT" means it's trying to establish an HTTPS connection
+            if (content is null || !content.StartsWith("CONNECT"))
+                return;
 
-                    await using var writer = new StreamWriter(clientStream);
-                    await writer.WriteLineAsync("HTTP/1.1 200 Connection established");
-                    await writer.WriteLineAsync($"Timestamp: {DateTime.UtcNow}");
-                    await writer.WriteLineAsync("Proxy-agent: Pixeval");
-                    await writer.WriteLineAsync();
-                    await writer.FlushAsync();
-                    var clientSsl = new SslStream(clientStream, false);
-                    // use specify certificate to establish the HTTPS connection
-                    await clientSsl.AuthenticateAsServerAsync(_certificate!, false, SslProtocols.None, false);
-                    // create an HTTP connection to the target IP
-                    var host = HostRegex().Match(content).Groups["host"].Value;
-                    var serverSsl = await CreateConnection(await _dns(host));
-                    var request = Functions.IgnoreExceptionAsync(() => clientSsl.CopyToAsync(serverSsl));
-                    var response = Functions.IgnoreExceptionAsync(() => serverSsl.CopyToAsync(clientSsl));
-                    _ = await Task.WhenAny(request, response);
-                    serverSsl.Close();
-                }
-            }
+            await using var writer = new StreamWriter(clientStream);
+            await writer.WriteLineAsync("HTTP/1.1 200 Connection established");
+            await writer.WriteLineAsync($"Timestamp: {DateTime.UtcNow}");
+            await writer.WriteLineAsync($"Proxy-agent: {nameof(Pixeval)}");
+            await writer.WriteLineAsync();
+            await writer.FlushAsync();
+            var clientSsl = new SslStream(clientStream, false);
+            // use specify certificate to establish the HTTPS connection
+            await clientSsl.AuthenticateAsServerAsync(_certificate, false, SslProtocols.None, false);
+            // create an HTTP connection to the target IP
+            var host = HostRegex().Match(content).Groups["host"].Value;
+            var serverSsl = await _createConnectionAsync(host);
+            var request = Functions.IgnoreExceptionAsync(() => clientSsl.CopyToAsync(serverSsl));
+            var response = Functions.IgnoreExceptionAsync(() => serverSsl.CopyToAsync(clientSsl));
+            _ = await Task.WhenAny(request, response);
+            serverSsl.Close();
         }
         catch
         {
             // ignore
-        }
-    }
-
-    private static async Task<SslStream> CreateConnection(IPAddress[] ipAddresses)
-    {
-        var client = new TcpClient();
-        await client.ConnectAsync(ipAddresses, 443);
-        var netStream = client.GetStream();
-        var sslStream = new SslStream(netStream, false, (_, _, _, _) => true);
-        try
-        {
-            await sslStream.AuthenticateAsClientAsync(string.Empty);
-            return sslStream;
-        }
-        catch
-        {
-            await sslStream.DisposeAsync();
-            throw;
         }
     }
 
