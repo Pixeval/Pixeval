4be400c703db8d064307841839072c0bf50c29c6
Shirasagi0012 <23011330+Shirasagi0012@users.noreply.github.com>
2026-04-20 04:13:14 +0800

    migrate WorkInfoPage to WorkInfoPane

--- src/Pixeval/Views/Viewers/WorkInfoPage.axaml.cs
+++ src/Pixeval/Views/Viewers/WorkInfoPage.axaml.cs
@@ -2,14 +2,7 @@
 // Licensed under the GPL-3.0 License.
 
 using Avalonia.Controls;
-using Avalonia.Interactivity;
-using Mako.Global.Enum;
-using Mako.Model;
 using Misaki;
-using Pixeval.AppManagement;
-using Pixeval.Models.Database.Managers;
-using Pixeval.Utilities;
-using Pixeval.Views.Capability;
 
 namespace Pixeval.Views.Viewers;
 
@@ -23,43 +16,5 @@ public partial class WorkInfoPage : ContentPage
     {
         DataContext = viewModel;
         InitializeComponent();
-    }
-
-    private async void AuthorButton_OnClick(object? sender, RoutedEventArgs e)
-    {
-        if (sender is not Control { DataContext: IUser user })
-            return;
-        if (TopLevel.GetTopLevel(this) is not { Launcher: { } launcher, ViewContainer: { } viewContainer })
-            return;
-
-        if (user is UserInfo info)
-            await viewContainer.CreateUserPageAsync(info.Id);
-        else
-            await launcher.LaunchUriAsync(user.WebsiteUri);
-    }
-
-    private void WorkTagButton_OnClicked(object? sender, RoutedEventArgs e)
-    {
-        if (DataContext is not IWorkEntry entry)
-            return;
-        if (sender is not Control { DataContext: ITag tag })
-            return;
-        if (TopLevel.GetTopLevel(this)?.ViewContainer is not { } viewContainer)
-            return;
-        var type = entry is Illustration ? SimpleWorkType.IllustrationAndManga : SimpleWorkType.Novel;
-        SearchHistoryPersistentManager.AddHistory(tag.Name, tag.TranslatedName);
-        viewContainer.NavigateTo(new SearchWorksPage(type, tag.Name));
-    }
-
-    private void BlockTag_OnClick(object? sender, RoutedEventArgs e)
-    {
-        if (sender is not Control { DataContext: ITag tag })
-            return;
-        var blockedTags = App.AppViewModel.AppSettings.BlockedTags;
-        if (!blockedTags.Contains(tag.Name))
-        {
-            blockedTags.Add(tag.Name);
-            AppInfo.SaveSettings(App.AppViewModel.AppSettings);
-        }
     }
 }
