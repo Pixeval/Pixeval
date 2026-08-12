5b46743becccc594cea42e3b601a057647dc53d6
Poker <poker_sang@outlook.com>
2026-08-13 01:44:37 +0800

    LanguageSettingsValue 模板

--- src/Pixeval/Views/Settings/LanguageSettingsValue.axaml.cs
+++ src/Pixeval/Views/Settings/LanguageSettingsValue.axaml.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Pixeval.
 // Licensed under the GPL-3.0 License.
 
+using System;
 using AutoSettingsPage.Avalonia;
 using AutoSettingsPage.Models;
 using Avalonia.Controls;
@@ -9,6 +10,8 @@ namespace Pixeval.Views.Settings;
 
 public partial class LanguageSettingsValue : ComboBox, IEntryControl<ISingleValueSettingsEntry<string>>
 {
+    protected override Type StyleKeyOverride => typeof(ComboBox);
+
     public ISingleValueSettingsEntry<string> Entry { set => DataContext = value; }
 
     public LanguageSettingsValue() => InitializeComponent();
